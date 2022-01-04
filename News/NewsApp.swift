2c1a477913032c2c89c7a88f22b89e13bd178ebb
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-01-04 16:00:14 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Paul Odhiambo on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
